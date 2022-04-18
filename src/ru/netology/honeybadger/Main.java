44324208f3b59cf36eeb04f2390e0361dc71c2e8
Dmitry Ushakov <ushakov-dp@mail.ru>
2022-04-18 15:03:33 +0700

    Fix bag

--- src/ru/netology/honeybadger/Main.java
+++ src/ru/netology/honeybadger/Main.java
@@ -3,27 +3,28 @@ package ru.netology.honeybadger;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class Main {
-    private static String path1 = "G://Games/savegames/save1.dat";
-    private static String path2 = "G://Games/savegames/save2.dat";
-    private static String path3 = "G://Games/savegames/save3.dat";
+    private static List<String> arrayPath = Arrays.asList("G://Games/savegames/save1.dat",
+            "G://Games/savegames/save2.dat",
+            "G://Games/savegames/save3.dat");
+    private static List<GameProgress> arrayGameProgress = new ArrayList<>();
 
     public static void main(String[] args) {
-//1. Произвести распаковку архива в папке savegames.
-        openZip("G://Games/savegames/save1.dat.zip", path1);
-        openZip("G://Games/savegames/save2.dat.zip", path2);
-        openZip("G://Games/savegames/save3.dat.zip", path3);
-//2. Произвести считывание и десериализацию одного из разархивированных файлов save.dat.
-        GameProgress gameProgress1 = openProgress(path1);
-        GameProgress gameProgress2 = openProgress(path2);
-        GameProgress gameProgress3 = openProgress(path3);
-//3. Вывести в консоль состояние сохранненой игры.
-        System.out.println(gameProgress1.toString());
-        System.out.println(gameProgress2.toString());
-        System.out.println(gameProgress3.toString());
+
+        for (int i = 0; i < arrayPath.size(); i++) {
+            //1. Произвести распаковку архива в папке savegames.
+            openZip(arrayPath.get(i) + ".zip", arrayPath.get(i));
+            //2. Произвести считывание и десериализацию одного из разархивированных файлов save.dat.
+            arrayGameProgress.add(openProgress(arrayPath.get(i)));
+            //3. Вывести в консоль состояние сохранненой игры.
+            System.out.println(arrayGameProgress.get(i).toString());
+        }
     }
 
     private static GameProgress openProgress(String path) {
